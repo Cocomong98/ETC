8077dfbcdaa7f64334bf13280e994d3a8a9a7c97
Kim yong hyeon <90203932+Cocomong98@users.noreply.github.com>
2024-12-03 23:52:37 +0900

    Operator

---
+++ a.js
@@ -0,0 +1,10 @@
+// 문자열 출력
+console.log("Hello World");
+// 숫자 출력
+console.log(1);
+// 논리식 출력
+console.log(true);
+// 객체 출력
+console.log({ name: "홍길동", age: 20, married: false });
+// 문자열 출력
+console.log("Hi!", 100, true, [1, 2, 3]);
